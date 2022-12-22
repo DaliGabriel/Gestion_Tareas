c6ecda70a3019c327889f6be1f3b58ab01700ff4
Gabriel Dalí <94218227+DaliGabriel@users.noreply.github.com>
2022-12-21 18:28:47 -0600

    traducir login y register

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,11 +12,8 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                     <x-nav-link :href="route('tareas.index')" :active="request()->routeIs('tareas.index')">
-                        {{ __('tareas') }}
+                        {{ __('Tareas') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -70,11 +67,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('tareas.index')" :active="request()->routeIs('tareas.index')">
-                {{ __('tareas') }}
+                {{ __('Tareas') }}
             </x-responsive-nav-link>
         </div>
 
